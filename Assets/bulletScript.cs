9fce59d74d905c82c2c1c58139b75b86cad25aa2
LiamDCreator <Liamduijn@gmail.com>
2024-03-12 18:21:44 +0100

    bullet timer

--- Assets/bulletScript.cs
+++ Assets/bulletScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class bulletScript : MonoBehaviour
 {
     // Start is called before the first frame update
-   
+   public float bulletTime;
     void Start()
     {
         
@@ -20,6 +20,15 @@ public class bulletScript : MonoBehaviour
         {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
         }
+    }
+
+    private void Update(){
+
+          bulletTime -= Time.deltaTime;
+          if(bulletTime <= 0){
+
+             Destroy(this.gameObject);
+          }
     }
       private void OnCollisionEnter2D(Collision2D collision){
   if (collision.gameObject.tag != "Player" && collision.gameObject.tag != "bullet")
