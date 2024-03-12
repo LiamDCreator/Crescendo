d017bf9e79d18a491140b115c5248a68118dd42f
LiamDCreator <Liamduijn@gmail.com>
2024-03-12 18:16:34 +0100

    player shoot direction

--- Assets/playerShoot.cs
+++ Assets/playerShoot.cs
@@ -4,47 +4,45 @@ using UnityEngine;
 
 public class playerShoot : MonoBehaviour
 {
+    
+     public float shootSpeed;
+
       public GameObject bulletPrefab; // The prefab of the bullet object
-      public float shootSpeed;
     public Transform shootPoint; // The point from which the player will shoot
-    private Vector2 leftRight;
+
+    private Vector2 shootDirection = Vector2.right; // Default shooting direction
+
     // Update is called once per frame
     void Update()
     {
-
-          if (Input.GetKeyDown(KeyCode.D)){
-                leftRight = Vector2.right;
-            }
-            
-         if (Input.GetKeyDown(KeyCode.A)){
-                leftRight = Vector2.left;
-            }
+        // Update the shoot direction based on movement keys
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            shootDirection = Vector2.right;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            shootDirection = Vector2.left;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            shootDirection = Vector2.up;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            shootDirection = Vector2.down;
+        }
 
         // Check for shooting input
         if (Input.GetKeyDown(KeyCode.G))
         {
-            // Determine the direction based on the last movement
-            Vector2 shootDirection = Vector2.zero;
-            if (Input.GetKey(KeyCode.D))
-                shootDirection = Vector2.right;
-                
-            
-            else if (Input.GetKey(KeyCode.A))
-                shootDirection = Vector2.left;
-               
-
-            else if (Input.GetKey(KeyCode.W))
-                shootDirection = Vector2.up;
-            else if (Input.GetKey(KeyCode.S))
-                shootDirection = Vector2.down;
-            else
-                shootDirection = leftRight; // Default shooting direction
-
             // Shoot in the determined direction
             Shoot(shootDirection);
         }
     }
-
     void Shoot(Vector2 direction)
     {
         // Instantiate bullet at the shoot point
@@ -54,6 +52,6 @@ public class playerShoot : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
 
         // Apply force to the bullet in the specified direction
-        rb.velocity = direction * shootSpeed; // Adjust the speed as needed
+        rb.velocity = shootDirection * shootSpeed; // Adjust the speed as needed
     }
 }
