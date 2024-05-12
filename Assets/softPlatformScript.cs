df375e4f070a11373b013b7ab50b073056086cf7
LiamDCreator <Liamduijn@gmail.com>
2024-05-12 13:58:04 +0200

    softplatform fixed player2

--- Assets/softPlatformScript.cs
+++ Assets/softPlatformScript.cs
@@ -6,6 +6,7 @@ public class softPlatformScript : MonoBehaviour
 {
    private Rigidbody2D rb;
     public string playerTag = "player";
+    public string player2Tag = "player2";
 
  
     void Start()
@@ -19,5 +20,9 @@ public class softPlatformScript : MonoBehaviour
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), 
                                   GameObject.FindGameObjectWithTag(playerTag).GetComponent<Collider2D>(), 
                                   transform.position.y > GameObject.FindGameObjectWithTag(playerTag).transform.position.y);
+                                  
+         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), 
+                                  GameObject.FindGameObjectWithTag(player2Tag).GetComponent<Collider2D>(), 
+                                  transform.position.y > GameObject.FindGameObjectWithTag(player2Tag).transform.position.y);                          
     }
 }
