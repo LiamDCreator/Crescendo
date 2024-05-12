2e210253d73b119e6a642319daae7ee592b7375d
LiamDCreator <Liamduijn@gmail.com>
2024-05-12 14:00:06 +0200

    Update playerScript.cs

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class playerScript : MonoBehaviour
 {
-      public float moveSpeed = 5f; // Adjust the speed of movement in Unity inspector if needed.
-    public float jumpForce = 10f; // Adjust the jump force in Unity inspector if needed.
+      public float moveSpeed = 5f; 
+    public float jumpForce = 10f; 
     private Rigidbody2D rb;
     private bool isGrounded;
     public Transform groundCheck;
@@ -33,7 +33,7 @@ public class playerScript : MonoBehaviour
        
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Set vertical velocity to jump force
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce); // jump
             crescendoCount += 1;
            
         }
